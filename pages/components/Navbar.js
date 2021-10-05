a9af69d0ac6bfde4b739038823738e231d934235
yudejp <i@yude.jp>
2021-10-05 22:44:15 +0900

    Update Navbar

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -1,26 +1,17 @@
 // Bootstrap
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 
 const NavbarComponent = () => {
     return (
         <Navbar bg="dark" expand="lg" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">𝑨 𝑴𝒊𝒏𝒆𝒄𝒓𝒂𝒇𝒕 𝑺𝒆𝒓𝒗𝒆𝒓</Navbar.Brand>
+            <Navbar.Brand href="#home">HunetMC</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="me-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#link">Link</Nav.Link>
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                  <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
               </Nav>
             </Navbar.Collapse>
           </Container>
