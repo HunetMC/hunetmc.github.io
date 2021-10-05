700c0200f48b325ad1ce3cbaa97793e56227b188
yudejp <i@yude.jp>
2021-10-05 22:20:14 +0900

    Add TumblrPost.js

---
+++ pages/api/TumblrPost.js
@@ -0,0 +1,30 @@
+const {
+    TUMBLR_API_KEY: api_key
+  } = process.env;
+
+
+export const TumblrAPI = async () => {
+  return fetch('https://api.tumblr.com/v2/blog/skuratech.tumblr.com/posts?api_key=' + api_key);
+};
+
+const TumblrPost = async (req, res) => {
+    const response = await TumblrAPI();
+    const data = await response.json();
+    if (response.status === 204 || response.status > 400) {
+      return res.status(200).send("404");
+    }
+    // const posts = data.response.posts;
+    const posts = data.response.posts.map((item) => {
+        var obj = new Object();
+        obj.date = item.date;
+        obj.url = item.post_url;
+        obj.title = item.title;
+        obj.body = item.body;
+        return obj
+    })
+    return res.status(200).json({
+        posts,
+    });
+};
+
+export default TumblrPost
