11c8cefaf9f4734c947a54bb2859a1b4c399eef2
yudejp <i@yude.jp>
2021-10-07 15:40:38 +0900

    Implement activeKey

--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -7,16 +7,38 @@ import Container from 'react-bootstrap/Container';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHome, faSearch } from '@fortawesome/free-solid-svg-icons'
 
+// React, Next.js Router
+import React from 'react';
+import { useRouter } from 'next/router';
+
 const NavbarComponent = () => {
+    const router = useRouter();
+    var currentActiveKey;
+    if (router.pathname.includes("players")) {
+      currentActiveKey = "/players"
+    } else {
+      if (router.pathname === "/") {
+        currentActiveKey = "/"
+      } else {
+        currentActiveKey = "NULL"
+      }
+    }
+    
     return (
         <Navbar bg="dark" expand="lg" variant="dark">
           <Container>
             <Navbar.Brand href="#home">HunetMC</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                <Nav.Link href="/"><FontAwesomeIcon icon={faHome} />&nbsp;ホーム</Nav.Link>
-                <Nav.Link href="/"><FontAwesomeIcon icon={faSearch} />&nbsp;プレイヤー検索</Nav.Link>
+              <Nav className="me-auto" activeKey={currentActiveKey}>
+                <Nav.Link href="/">
+                  <FontAwesomeIcon icon={faHome} width={20} />&nbsp;
+                  ホーム
+                </Nav.Link>
+                <Nav.Link href="/players">
+                  <FontAwesomeIcon icon={faSearch} width={20} />&nbsp;
+                  プレイヤー検索
+                </Nav.Link>
               </Nav>
             </Navbar.Collapse>
           </Container>
