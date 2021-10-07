fb479554768b3e9a9a6bf6d3a6e8ee20112e78b8
yudejp <i@yude.jp>
2021-10-07 14:37:59 +0900

    Add typeahead on `/players`

--- pages/players.js
+++ pages/players.js
@@ -3,12 +3,10 @@ import Card from 'react-bootstrap/Card';
 
 // Data fetching
 import useSwr from 'swr'
+const fetcher = (url) => fetch(url).then((res) => res.text())
 
-// Data fetching implements
-const fetcher = (url) => fetch(url).then((res) => res.json())
-
-// Next.js
-import Link from "next/link"
+// React
+import React, { useState, Fragment } from 'react';
 
 // Bootstrap
 import Col from 'react-bootstrap/Col';
@@ -23,24 +21,52 @@ import 'react-bootstrap-typeahead/css/Typeahead.css';
 import Layout from './components/Layout'
 
 function App () {
-    return (
-        <Layout title="プレイヤー検索">
-          <Container style={{ 'marginTop': '100px' }} className="mx-auto">
-             <h1 className="text-center">プレイヤー検索</h1>
-             <Form className="mx-auto" style={{ 'max-width' : '600px' }}>
-              <Row className="align-items-center">
-                <Col className="my-1">
-                  <Form.Control size="lg" id="inlineFormInputName" placeholder="プレイヤー名を入力してください。" />
-                </Col>
-                <Col xs="auto" className="my-1">
-                  <Button size="lg" type="submit">検索</Button>
-                </Col>
-              </Row>
-            </Form>
-          </Container>
-        </Layout>
-        
+    const { data, error } = useSwr(
+        `https://api.skura.tech/players/list`,
+        fetcher
     )
+    const [singleSelections, setSingleSelections] = useState([]);
+    
+    if (error) {
+        return (
+          <p>API サーバーがダウンしています。しばらくお待ちください。</p>
+        )
+      } else {
+        if (!data) {
+          return (
+            <>読み込み中...</>
+          )
+        } else {
+            var array = JSON.parse(data);
+            return (
+                <Fragment>
+                <Layout title="プレイヤー検索">
+                  <Container style={{ 'marginTop': '100px' }} className="mx-auto">
+                     <h1 className="text-center">プレイヤー検索</h1>
+                     <Form className="mx-auto" style={{ 'max-width' : '600px' }}>
+                      <Row className="align-items-center">
+                        <Col className="my-1">
+                            <Typeahead
+                              size="lg"
+                              id="basic-typeahead-single"
+                              labelKey="name"
+                              onChange={setSingleSelections}
+                              options={array}
+                              placeholder="プレイヤー名を入力してください。"
+                              selected={singleSelections}
+                            />
+                        </Col>
+                        <Col xs="auto" className="my-1">
+                          <Button size="lg" type="submit">検索</Button>
+                        </Col>
+                      </Row>
+                    </Form>
+                  </Container>
+                </Layout>
+                </Fragment>
+            )
+        }
+      }
 }
   
 export default App;
